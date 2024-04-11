65b99b8849552cda4cdb20ffcf28ad03ccbeef1a
juanescan <juanestebancanelado63@gmail.com>
2024-04-11 00:55:02 -0500

    algunas pruebas

--- Spiderweb/Pruebas/spiderwebc2test.java
+++ Spiderweb/Pruebas/spiderwebc2test.java
@@ -1,10 +1,11 @@
 package Pruebas;
 
 
-
+import spiderweb.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Before;
+import org.junit.After;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -15,21 +16,58 @@ import org.junit.jupiter.api.Test;
  */
 public class spiderwebc2test
 {
-    /**
-     * Default constructor for test class spiderwebc2test
-     */
-    public spiderwebc2test()
-    {
-    }
+   
 
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @BeforeEach
-    public void setUp()
-    {
+    @Test
+    public void testAddBridge() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.addBridge("red", 50, 1); // Example parameters
+        assertTrue(web.ok());
+    }
+    
+    @Test
+    public void testAddSpot() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.addSpot("blue", 2); // Example parameters
+        assertTrue(web.ok());
+    }
+    
+    @Test
+    public void testDelBridge() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.addBridge("blue", 100, 1);
+        web.delBridge("blue");
+        assertTrue(web.ok());
+    }
+    
+    @Test
+    public void testDelSpot() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.addSpot("red", 4);
+        web.delSpot("red");
+        assertTrue(web.ok());
+    }
+    
+    @Test
+    public void testSpiderWalk() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.spiderSit(1);
+        web.spiderWalk(true); // Example parameter
+        assertTrue(web.ok());
+    }
+    
+    @Test
+    public void testMakeVisible() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.makeVisible();
+        assertTrue(web.ok());
+    }
+    
+    @Test
+    public void testMakeInvisible() {
+        spiderWeb web = new spiderWeb(5, 150);
+        web.makeInvisible();
+        assertTrue(web.ok());
     }
 
     /**
